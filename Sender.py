7c40036a56db638d3a16f5f170bace04a6183e19
Peilun(Mark) Xi <pxi10@berkeley.edu>
2015-10-24 23:38:13 -0700

    implemented fast retransmit and SACK

--- Sender.py
+++ Sender.py
@@ -38,6 +38,13 @@ class Sender(BasicSender.BasicSender):
 
         next_send_seq = sequence_num-1
         END = False
+
+        # Used to determine the exact packets to send in SACK Mode
+        lastAckPacket = None
+
+        lastAckSqn = None
+        sameAckCount = 0
+
         while True:
             if self.debug:
                 print "now window size--------> ", len(window)
@@ -66,7 +73,39 @@ class Sender(BasicSender.BasicSender):
             res = self.receive(0.5)
 
             if res and Checksum.validate_checksum(res):             # if sender not receving any ack, this will continue to next loop and send the whole window again
-                expected_next_seq = int(self._split_message(res)[1])     # if sender receives some ack, this will move the window
+                
+                lastAckPacket = res
+                print "received sth", lastAckPacket
+
+                if sackMode:
+                    expected_next_seq = long(((self._split_message(res)[1]).split(';'))[0])     # if sender receives some ack, this will move the window
+                else:
+                    expected_next_seq = long(self._split_message(res)[1])
+
+                # Retransmit upon receiving more than 3 identical acks.
+                if lastAckSqn == expected_next_seq:
+                    sameAckCount += 1
+                    if sameAckCount >= 4:
+                        if sackMode:
+                            #Delete received packets from window.
+                            sack_msg = (self.split_packet(res)[1]).split(';')
+                            msg_to_send = {}
+
+                            for i in range(7):
+                                msg_to_send[long(sack_msg[0])+i] = 1
+                            for i in sack_msg[1]:
+                                msg_to_send.pop(long(i))
+
+                            for packet in window:
+                                if packet[0] in msg_to_send:
+                                    self.send(packet[1])
+
+                    
+                        self.send(window[0][1])
+                else:
+                    lastAckSqn = expected_next_seq
+                    sameAckCount = 0
+
                 new_window = []
                 for d in window:
                     # print "*****************",  d[0]>=expected_next_seq
@@ -78,7 +117,22 @@ class Sender(BasicSender.BasicSender):
                 if self.debug:
                     print "<<< RECEIVE ACK: ", res
             else:
-                next_send_seq = window[0][0]-1        # when no acks recevied on sender side, reset expected_next_seq to send the whole window
+                if sackMode:
+                    #Delete received packets from window.
+                    sack_msg = (self.split_packet(lastAckPacket)[1]).split(';')
+                    msg_to_send = {}
+
+                    for i in range(7):
+                        msg_to_send[long(sack_msg[0])+i] = 1
+                    for i in sack_msg[1].split(','):
+                        msg_to_send.pop(long(i))
+
+                    for packet in window:
+                        if packet[0] in msg_to_send:
+                            self.send(packet[1])
+                else:
+                    next_send_seq = window[0][0]-1        # when no acks recevied on sender side, reset expected_next_seq to send the whole window
+
 
             if END and window==[]:
                 break
